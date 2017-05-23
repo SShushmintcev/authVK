49aa9c19c30ee75a1e75f1d164230c834c2dd51d
Святослав <small85@mail.ru>
2017-05-23 15:25:02 +0300

    Add constructor

--- OAuthVk/AuthVk.cs
+++ OAuthVk/AuthVk.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace OAuthVk
 {
-    public class AuthVk
+  public class AuthVk
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthVk" /> class.
+    /// </summary>
+    public AuthVk()
     {
     }
+  }
 }
